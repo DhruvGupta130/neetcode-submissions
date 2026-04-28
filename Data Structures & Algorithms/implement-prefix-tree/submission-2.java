d21bc1046b7b0ab87e342bd312b1bc0a84e1d500
Dhruv Gupta <89991050+DhruvGupta130@users.noreply.github.com>
2026-04-29 03:22:30 +0530

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.java
@@ -0,0 +1,50 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEndOfWord = false;
+    }
+}
+class PrefixTree {
+    private TrieNode root;
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+            curr = curr.children[index];
+        }
+        curr.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = findNode(word);
+        return node != null && node.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        return findNode(prefix) != null;
+    }
+
+    private TrieNode findNode(String str) {
+        TrieNode curr = root;
+
+        for (char ch : str.toCharArray()) {
+            int index = ch - 'a';
+            if (curr.children[index] == null) {
+                return null;
+            }
+            curr = curr.children[index];
+        }
+        return curr;
+    }
+}
