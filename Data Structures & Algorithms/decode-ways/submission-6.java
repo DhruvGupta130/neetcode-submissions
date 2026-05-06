18723381fdf588c545488048f6410a88189c9673
Dhruv Gupta <89991050+DhruvGupta130@users.noreply.github.com>
2026-05-07 01:51:29 +0530

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+
+        if (n == 0 || s.charAt(0) == '0')
+            return 0;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int one = s.charAt(i - 1) - '0';
+            int two = (s.charAt(i - 2) - '0') * 10 + one;
+
+            if (one > 0) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (two >= 10 && two <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+}
