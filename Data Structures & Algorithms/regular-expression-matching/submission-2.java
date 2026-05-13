8f3dce98d834f56442d4a8ec447746430b1c75bc
Dhruv Gupta <89991050+DhruvGupta130@users.noreply.github.com>
2026-05-14 00:59:18 +0530

    Add: regular-expression-matching - submission-2

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        char[] c = s.toCharArray();
+        char[] d = p.toCharArray();
+
+        Boolean[][] dp = new Boolean[c.length + 1][d.length + 1];
+        return check(c, 0, d, 0, dp);
+    }
+
+    private boolean check(char[] c, int i, char[] d, int j, Boolean[][] dp) {
+        if (j == d.length)
+            return i == c.length;
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        boolean match = i < c.length && (c[i] == d[j] || d[j] == '.');
+
+        if (j + 1 < d.length && d[j + 1] == '*') {
+            return dp[i][j] = check(c, i, d, j + 2, dp) || (match && check(c, i + 1, d, j, dp));
+        } else {
+            return dp[i][j] = match && check(c, i + 1, d, j + 1, dp);
+        }
+    }
+}
